b2a14e375fc9642f38a4e3f9c24f12cf13e2ce43
Numan506 <78308167+Numan506@users.noreply.github.com>
2021-03-02 15:17:35 +0600

    Add files via upload

---
+++ Call_by_Reference.java
@@ -0,0 +1,15 @@
+
+package oop;
+
+
+public class Call_by_Reference {
+    
+    String name,r2;
+    
+    
+    void change(Call_by_Reference r2){//r2 = Jakaria
+    
+        r2.name = "koyes";//jehetu Call_by_Reference same method make kora hoyese,so j kunu ek jaigay poriborton hole tha onno jaigay o hobe
+    
+    }
+}
